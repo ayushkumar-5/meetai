ba91244a5d6a4de2181ad01a40ecac2d5ccd5b09
ayushkumar-5 <justinrawaat@gmail.com>
2025-08-01 12:10:56 +0530

    Update dependencies and integrate TRPC; add new HomeView component for improved dashboard functionality

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,38 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
-type User = { name: string };
-type Session = { user: User } | null;
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const router = useRouter();
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      const sessionData = await authClient.getSession();
-      if (sessionData && 'user' in sessionData) {
-        setSession(sessionData as Session);
-      } else {
-        setSession(null);
-      }
-    };
-    fetchSession();
-  }, []);
-
-  if (!session || !session.user) {
-    return <p>Loading...</p>;
-  }
-
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Ayushh" }));
   return (
     <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-      <Button onClick={() => authClient.signOut({fetchOptions: {onSuccess: () => router.push("/sign-in")}})}>
-        Sign out
-      </Button>
+      {data?.greeting}
     </div>
   );
 };
