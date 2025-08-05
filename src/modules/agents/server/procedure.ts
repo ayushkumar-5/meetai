574427e5e6eb58df18a4d07ce7b3f6e02e669186
ayushkumar-5 <justinrawaat@gmail.com>
2025-08-05 11:51:03 +0530

    Add agent details view: implement agent ID page with loading and error states, and create header component for agent actions. Update server procedure to include user authorization for agent retrieval.

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -5,16 +5,20 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter , protectedProcedure} from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import {DEFAULT_PAGE, MIN_PAGE_SIZE, MAX_PAGE_SIZE, DEFAULT_PAGE_SIZE} from "@/constants";
-// import {TRPCError} from "@trpc/server";
+import {TRPCError} from "@trpc/server";
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
     const [existingAgent] = await db
       .select({
         meetingCount: sql<number>`5`,
         ...getTableColumns(agents),
       })
       .from(agents)
-      .where(eq(agents.id, input.id));
+      .where(and(eq(agents.id, input.id),eq(agents.userId, ctx.auth.user.id),));
+
+      if (!existingAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+        }
     return existingAgent;
   }),
 
